55070cfaded27235f4ee24c94c577461b69a8122
MOHAMMED MISHAL <139131510+Mishalma@users.noreply.github.com>
2025-08-25 09:20:22 +0530

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-# test_pdf.py
-import asyncio
-from src.utils.pdf_generator import generate_pdf
-async def test():
-    path = await generate_pdf("നിന്റെ പേര് എന്താണ്?", "test_ml.pdf", language="ml")
-    print(path)
-asyncio.run(test())
